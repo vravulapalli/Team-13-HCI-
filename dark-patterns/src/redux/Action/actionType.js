425ed21251c53d9a1fa3ee27ceec86a9b37c9548
vravulapalli <vineetha.ravulapalli@slu.edu>
2024-04-15 12:03:58 -0500

    deleting from main

--- dark-patterns/src/redux/Action/actionType.js
+++
@@ -1,9 +0,0 @@
-// authActions --------------------------------
-export const LOGIN = "LOGIN";
-export const LOGOUT = "LOGOUT";
-export const USER_DATA = "USER_DATA";
-
-export const USERS_LIST = "USERS_LIST";
-
-// websites --------------------------------
-export const WESBITES_LIST = "WESBITES_LIST";
