6bba9bf0f1a291c15ef6db81e74a79dc5a88a97b
bkarakala <123421146+bkarakala@users.noreply.github.com>
2024-04-15 23:56:07 -0500

    App test update

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dark Pattern Detected!</h1>
+      <p>Be cautious, the website might be trying to manipulate your shopping experience.</p>
+    </div>
+  );
+}
+
+export default App;
